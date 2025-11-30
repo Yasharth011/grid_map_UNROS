07a5c5d47c2ff3c3667dcdc2c8f62cf681d7bef4
Yasharth011 <yasharthgupta10@gmail.com>
2025-11-30 20:29:33 +0530

    nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "flake for an grid map library";
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+          buildInputs = with pkgs; [
+	    boost 
+	    eigen
+	    yaml-cpp
+          ];
+        };
+
+      });
+}
+
